d643acb746f6467550d8c8afe9715faa765b3951
김진영C <jin0kim326@douzone.com>
2022-12-05 09:02:27 +0900

    react router 설정

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { Children } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './pages/ErrorPage';
+import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail';
+import Basket from './pages/Basket';
+import AddProduct from './pages/AddProduct';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index:true,
+        element: <Products />
+      },
+      {
+        path:"/products/:id",
+        element: <ProductDetail />
+      },
+      {
+        path: "/basket/:id",
+        element: <Basket />
+      },
+      {
+        path: "/product/add",
+        element: <AddProduct />
+      }
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ErrorPage() {
+    return (
+        <div>
+            🚑 Error ! 관리자에게 문의하세요 !
+        </div>
+    );
+}
+
