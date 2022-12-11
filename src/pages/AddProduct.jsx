6d6ed7f5242e4c94fe9cf5db49419ea62862bc2f
김진영C <jin0kim326@douzone.com>
2022-12-11 15:19:24 +0900

    14.14 상품 입력폼 디자인

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -1,10 +1,92 @@
-import React from 'react';
+import { Cloudinary } from '@cloudinary/url-gen';
+import React, { useState } from 'react';
+import Button from '../components/ui/Button';
+import { useEffect } from 'react';
 
 export default function AddProduct() {
-    return (
-        <div>
-            addProduct
-        </div>
-    );
-}
+  const [product, setProduct] = useState({});
+  const [file, setFile] = useState();
 
+  const cld = new Cloudinary();
+  useEffect(() => {
+    console.log(cld.getConfig());
+  });
+
+  const handleChange = (e) => {
+    const { name, value, files } = e.target;
+    if (name === 'file') {
+      setFile(files && files[0]);
+      return;
+    }
+    setProduct((product) => ({ ...product, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // 제품의 사진을 Cloudinary에 업로드 -> url획득
+
+    const cloudinaryData = {};
+    cloudinaryData.append('file', file);
+    cloudinaryData.append('api_key');
+    cloudinaryData.append('public_id');
+    cloudinaryData.append('timestamp', new Date());
+    cloudinaryData.append('signature');
+
+    // Firebase에 새로운 제품 추가
+  };
+  return (
+    <section>
+      {file && <img src={URL.createObjectURL(file)} alt='local file' />}
+      <form onSubmit={handleSubmit}>
+        <input
+          type='file'
+          accept='image/*'
+          name='file'
+          required
+          onChange={handleChange}
+        />
+        <input
+          type='text'
+          name='title'
+          value={product.title ?? ''}
+          placeholder='제품명'
+          required
+          onChange={handleChange}
+        />
+        <input
+          type='number'
+          name='price'
+          value={product.price ?? ''}
+          placeholder='가격'
+          required
+          onChange={handleChange}
+        />
+        <input
+          type='text'
+          name='category'
+          value={product.category ?? ''}
+          placeholder='카테고리'
+          required
+          onChange={handleChange}
+        />
+        <input
+          type='text'
+          name='description'
+          value={product.description ?? ''}
+          placeholder='설명'
+          required
+          onChange={handleChange}
+        />
+        <input
+          type='text'
+          name='options'
+          value={product.options ?? ''}
+          placeholder='옵션들(콤마(,)로 구분)'
+          required
+          onChange={handleChange}
+        />
+        <Button text={'제품 등록하기'} />
+      </form>
+    </section>
+  );
+}
