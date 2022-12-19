b9f6b7e3a688dbeff903e4dd665d9cdcb79fe305
김진영C <jin0kim326@douzone.com>
2022-12-19 15:24:59 +0900

    useQuery에서 staletime이 설정되어 있을때 데이터 업데이트시 invalidate하기

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
-import Button from '../components/ui/Button';
-import { uploadImage } from '../config/uploader';
-import { addProduct } from '../config/firebase';
+import React, { useState } from "react";
+import Button from "../components/ui/Button";
+import { uploadImage } from "../config/uploader";
+import { addProduct } from "../config/firebase";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export default function AddProduct() {
   const [product, setProduct] = useState({});
@@ -9,9 +10,15 @@ export default function AddProduct() {
   const [isUploading, setIsUploading] = useState(false);
   const [success, setSuccess] = useState();
 
+  const queryClient = useQueryClient();
+  const addProduct = useMutation(
+    ({ product, url }) => addProduct(product, url),
+    { onSuccess: () => queryClient.invalidate(["products"]) }
+  );
+
   const handleChange = (e) => {
     const { name, value, files } = e.target;
-    if (name === 'file') {
+    if (name === "file") {
       setFile(files && files[0]);
       return;
     }
@@ -24,13 +31,17 @@ export default function AddProduct() {
 
     uploadImage(file)
       .then((url) => {
-        addProduct(product, url) //
-          .then(() => {
-            setSuccess('성공적으로 제품이 추가되었습니다.');
-            setTimeout(() => {
-              setSuccess(null);
-            }, 4000);
-          });
+        addProduct.mutate(
+          { product, url },
+          {
+            onSuccess: () => {
+              setSuccess("성공적으로 제품이 추가되었습니다.");
+              setTimeout(() => {
+                setSuccess(null);
+              }, 4000);
+            },
+          }
+        );
       })
       .finally(() => setIsUploading(false));
 
@@ -39,66 +50,66 @@ export default function AddProduct() {
     // Firebase에 새로운 제품 추가
   };
   return (
-    <section className='w-full text-center'>
-      <h2 className='text-2xl font-bold my-4'>새로운 제품 등록</h2>
-      {success && <p className='my-2'>✅ {success} </p>}
+    <section className="w-full text-center">
+      <h2 className="text-2xl font-bold my-4">새로운 제품 등록</h2>
+      {success && <p className="my-2">✅ {success} </p>}
       {file && (
         <img
-          className='w-96 mx-auto mb-2'
+          className="w-96 mx-auto mb-2"
           src={URL.createObjectURL(file)}
-          alt='local file'
+          alt="local file"
         />
       )}
-      <form className='flex flex-col px-12' onSubmit={handleSubmit}>
+      <form className="flex flex-col px-12" onSubmit={handleSubmit}>
         <input
-          type='file'
-          accept='image/*'
-          name='file'
+          type="file"
+          accept="image/*"
+          name="file"
           required
           onChange={handleChange}
         />
         <input
-          type='text'
-          name='title'
-          value={product.title ?? ''}
-          placeholder='제품명'
+          type="text"
+          name="title"
+          value={product.title ?? ""}
+          placeholder="제품명"
           required
           onChange={handleChange}
         />
         <input
-          type='number'
-          name='price'
-          value={product.price ?? ''}
-          placeholder='가격'
+          type="number"
+          name="price"
+          value={product.price ?? ""}
+          placeholder="가격"
           required
           onChange={handleChange}
         />
         <input
-          type='text'
-          name='category'
-          value={product.category ?? ''}
-          placeholder='카테고리'
+          type="text"
+          name="category"
+          value={product.category ?? ""}
+          placeholder="카테고리"
           required
           onChange={handleChange}
         />
         <input
-          type='text'
-          name='description'
-          value={product.description ?? ''}
-          placeholder='설명'
+          type="text"
+          name="description"
+          value={product.description ?? ""}
+          placeholder="설명"
           required
           onChange={handleChange}
         />
         <input
-          type='text'
-          name='options'
-          value={product.options ?? ''}
-          placeholder='옵션들(콤마(,)로 구분)'
+          type="text"
+          name="options"
+          value={product.options ?? ""}
+          placeholder="옵션들(콤마(,)로 구분)"
           required
           onChange={handleChange}
         />
         <Button
-          text={isUploading ? '업로드중' : '제품 등록하기'}
+          text={isUploading ? "업로드중" : "제품 등록하기"}
           disabled={isUploading}
         />
       </form>
